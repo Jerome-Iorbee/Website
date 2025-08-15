5dfbed950220df529d318cd68b471585918313da
Jerome-Iorbee <jezmaniorbee@gmail.com>
2025-08-15 01:51:11 +0100

    Update header.php

--- header.php
+++ header.php
@@ -1,3 +1,81 @@
+<style>
+nav{
+    margin-top: -1.2%;
+    margin-left: -1.2%;
+}
+ul{
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    background-color: red;
+    overflow: hidden;
+    gap: 2.5%;
+    padding: 1%;
+}
+li{
+    list-style-type: none;
+}
+li a{
+    text-decoration: none;
+    color: white;
+    font-weight: bolder;
+}
+.deals{
+    display: flex;
+    gap: 2%;
+    flex-direction: row;
+    width: 100%;
+}
+.deals div{
+    width: 100%;
+}
+.deals img{
+    width: 100%;
+}
+.deals p{
+    text-align: justify;
+    width: 100%;
+}
+@media only screen and (max-width: 600px){
+    nav{
+        margin-top: -4.9%;
+        margin-left: -2.1%;
+    }
+    ul{
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        background-color: red;
+        overflow: hidden;
+        gap: 2.5%;
+        padding: 1%;
+    }
+    li{
+        list-style-type: none;
+    }
+    li a{
+        text-decoration: none;
+        color: white;
+        font-weight: bolder;
+    }
+    .deals{
+        display: flex;
+        gap: 2%;
+        flex-direction: column;
+        width: 100%;
+    }
+    .deals div{
+        width: 100%;
+    }
+    .deals img{
+        width: 100%;
+    }
+    .deals p{
+        text-align: justify;
+        width: 100%;
+    }
+}
+</style>
 <link rel="stylesheet" href="css/navflex.css">
 </head>
 <body>
